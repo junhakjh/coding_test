c36916024b8d79ee7123b506f47771be6e33495b
junhakjh <112874801+junhakjh@users.noreply.github.com>
2024-01-07 21:31:51 +0900

    Delete leetcode/BruteForce directory

--- leetcode/BruteForce/TwoSum.py
+++
@@ -1,9 +0,0 @@
-from itertools import combinations
-
-class Solution(object):
-    def twoSum(self, nums, target):
-        combination = combinations([i for i in range(len(nums))], 2)
-        for comb in combination:
-            a, b = comb[0], comb[1]
-            if nums[a] + nums[b] == target:
-                return [a, b]
