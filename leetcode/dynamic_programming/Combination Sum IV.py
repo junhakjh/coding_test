c574a370642dfb41adebcaf9a32eaca1dd6bb9ff
junhakjh <112874801+junhakjh@users.noreply.github.com>
2024-01-16 17:13:38 +0900

    [LeetCode] Combination Sum IV

---
+++ leetcode/dynamic_programming/Combination Sum IV.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        arr = [0 for _ in range(target + 1)]
+        for i in nums:
+            if i <= target:
+                arr[i] = 1
+        for i, cur_num in enumerate(arr):
+            print(arr)
+            if cur_num == 0:
+                continue
+            for num in nums:
+                if i + num <= target:
+                    arr[i + num] += cur_num
+        return arr[-1]
