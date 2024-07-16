0b0fdfa7116db31aeaf27bbfae406d67b0d024b9
Junha Kim <112874801+junhakjh@users.noreply.github.com>
2024-07-16 17:19:23 +0900

    [SWEA] 1926_간단한 369게임.java

--- SW_Expert_Academy/D2/1926_간단한 369게임.java
+++ SW_Expert_Academy/D2/1926_간단한 369게임.java
@@ -1,38 +1,28 @@
 import java.io.*;
-import java.util.*;
 
 public class Solution {
 	static final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static final StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
 	
-	static int solution(int[] candies) {
-		int answer = 0;
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(input.readLine());
 		
-		int prev = candies[2];
-		for(int i = 1; i >= 0; i--) {
-			if(candies[i] < prev) {
-				prev = candies[i];
-				continue;
+		for(int num = 1; num <= n; num++) {
+			String str = Integer.toString(num);
+			int cnt = 0;
+			for(int i = 0; i < str.length(); i++) {
+				char c = str.charAt(i);
+				if(c == '3' || c == '6' || c == '9') {
+					cnt++;
+					sb.append("-");
+				}
 			}
-			answer += candies[i] - (--prev);
-		}
-		
-		return prev <= 0 ? -1 : answer;
-	}
-	
-	public static void main(String[] args) throws Exception {	
-		int T = Integer.parseInt(input.readLine());
-		
-		
-		for(int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(input.readLine());
-			int[] candies = new int[3];
-			for(int i = 0; i < 3; i++) {
-				candies[i] = Integer.parseInt(st.nextToken());
+			if(cnt == 0) {
+				sb.append(str);
 			}
-			
-			sb.append("#").append(tc).append(" ").append(solution(candies)).append("\n");
+			if(num != n) {
+				sb.append(" ");
+			}
 		}
 		
 		System.out.println(sb.toString());
